f9d121ead4bbc6f9317d8d2e649e89a06c3d2b42
Don Benjamin <don@webhammer.co.uk>
2026-01-29 10:27:33 +0000

    Added SHORT_DNS_TTL environment variable

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Show dockerhub on http://localhost/docker and duckduckgo on http://localhost/duc
 ```
 docker run -p 80:80 -e PROXY_BACKEND_1=/docker:https://hub.docker.com -e PROXY_BACKEND_2=/duck:https://duckduckgo.com --rm donuk/httpurlbasedproxy
 ```
+
+If your backend proxies change ip frequently (e.g. if they are local services undergoing updates) you can use environment variable `SHORT_DNS_TTL=yes` to reduce dns caching times and prevent 502 errors.
--- setup-proxy-config.sh
+++ setup-proxy-config.sh
@@ -2,11 +2,15 @@
 
 main() {
     generate_nginx_proxy_conf > /etc/nginx/conf.d/default.conf
+    if [ "$SHORT_DNS_TTL" = "yes" ]; then
+        bypass_dns_cache
+    fi
 }
 
 list_redirects() {
     set | sed -n '/^PROXY_BACKEND\(_[0-9]\+\)\?=/ {s/[^=]*=//; s/:/ /; p;}' | sort -r
 }
+
 generate_nginx_proxy_conf() {
     cat <<EOF
 server {
@@ -18,6 +22,8 @@ EOF
         proxy_pass $target;
         proxy_pass_request_headers on;
         proxy_set_header Host \$host;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
     }
 EOF2
     }
@@ -28,4 +34,14 @@ EOF2
 EOF
 }
 
-main
+bypass_dns_cache() {
+    sed '
+    /^\s*http {$/ a\
+        resolver 127.0.0.11 valid=10s;  # Docker internal DNS\
+        resolver_timeout 5s;\
+    ' -i /etc/nginx/nginx.conf
+
+    cat /etc/nginx/nginx.conf
+}
+
+main "$@"
