f03af0932036e7a53e1664479a0b8ff995c0e42f
Don Benjamin <don@webhammer.co.uk>
2026-01-29 22:40:44 +0000

    nginx wasn't actually re-resolving the dns names

--- setup-proxy-config.sh
+++ setup-proxy-config.sh
@@ -2,7 +2,7 @@
 
 main() {
     generate_nginx_proxy_conf > /etc/nginx/conf.d/default.conf
-    if [ "$SHORT_DNS_TTL" = "yes" ]; then
+    if should_force_dns_refresh; then
         bypass_dns_cache
     fi
 }
@@ -19,7 +19,7 @@ EOF
     list_redirects | while read source target; do {
         cat <<EOF2
     location $source {
-        proxy_pass $target;
+$(proxy_pass_line "$target")
         proxy_pass_request_headers on;
         proxy_set_header Host \$host;
         proxy_set_header Upgrade \$http_upgrade;
@@ -33,11 +33,32 @@ EOF2
 }
 EOF
 }
+should_force_dns_refresh() {
+    if [ "$SHORT_DNS_TTL" = "yes" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+proxy_pass_line() {
+    target="$1"
+
+    if should_force_dns_refresh; then
+cat << EOF
+        set \$upstream "$target";
+        proxy_pass \$upstream;
+EOF
+    else
+cat << EOF
+        proxy_pass $target;
+EOF
+    fi
+}
 
 bypass_dns_cache() {
     sed '
     /^\s*http {$/ a\
-        resolver 127.0.0.11 valid=10s;  # Docker internal DNS\
+        resolver 127.0.0.11 1.1.1.1 8.8.8.8 valid=10s;  # Docker internal DNS\
         resolver_timeout 5s;\
     ' -i /etc/nginx/nginx.conf
 
